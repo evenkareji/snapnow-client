ce676dc2ac09a0533f693b553d5365690d27effc
github user name <yokota.tomo222282@gmail.com>
2023-11-27 17:17:29 +0900

    再deploy

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getPosts = async () => {
-  const response = await axios.get(`${process.env.API_URL}/posts`);
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
   return response.data.sort((post1, post2) => {
     return (
       new Date(post2.createdAt).valueOf() - new Date(post1.createdAt).valueOf()
